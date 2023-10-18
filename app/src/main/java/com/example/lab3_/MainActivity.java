fd7467c14daf4cb0be22473d48d54ab5f21055e5
Adam Grace <S00188523@atu.ie>
2023-10-18 23:43:16 +0100

    new xml and java files added

--- app/src/main/java/com/example/lab3_/MainActivity.java
+++ app/src/main/java/com/example/lab3_/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.lab3_;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rockNRollButton = findViewById(R.id.button);
+        Button hipHopButton = findViewById(R.id.button2);
+        Button sambaButton = findViewById(R.id.button3);
+
+        rockNRollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ChartActivity.class));
+            }
+        });
+
+        hipHopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ChartActivity.class));
+            }
+        });
+
+        sambaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ChartActivity.class));
+            }
+        });
     }
 }
